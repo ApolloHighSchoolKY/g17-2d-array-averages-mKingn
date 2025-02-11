a8ab589918fb863b4203275eee93fae8077d2d16
mKingn <michael.king@stu.daviess.kyschools.us>
2025-02-11 18:27:26 +0000

    y

--- ArrayAverages.java
+++ ArrayAverages.java
@@ -8,22 +8,70 @@ public class ArrayAverages{
     ray = rayray;
   }
 
-  public void rowAverages(){
-    
+  public void rowAverages()
+  {
+    int total=0;
+    int length;
+    for(int row[]: rayray)
+    {
+      for(int col: rayray[row])
+      {
+        total+=rayray[row][col];
+      }
+
+      length= rayray[row].length;
+      total = total / length;
+
+      System.out.println("the average of row " + row + " is " + total);
+      total = 0;
+      length = 0;
+    }
   }
 
-  public void columnAverages(){
+  public void columnAverages()
+  {
+    int total=0;
+    int length;
+    for(int col[]: rayray)
+    {
+      for(int row: rayray[col])
+      {
+        total+=rayray[col][row];
+      }
+
+      length= rayray[col].length;
+      total = total / length;
+
+      System.out.println("the average of collum " + col + " is " + total);
+      total = 0;
+      length = 0;
+    } 
+  }
+
+  public void arrayAverage()
+  {
+    int total=0;
+    int length=0; 
+    for(row=0; row <rayray.length; row++)
+    {
+      for(int col=0; col < rayray[row].length; col++ )
+      {
+        total+= rayray[row][col];
+        length++;
+      }
+    }
+    total = total / length;
+
+    System.out.println("the average of the array is " + total);
 
   }
 
-  public void arrayAverage(){
-
-  }
-
-  public String toString(){
+  public String toString()
+  {
     for(int row=0; row<ray.length; row++)
     {
       System.out.println(Arrays.toString(ray[row]));
     }
+    return "";
   }
 }
--- Gee17.java
+++ Gee17.java
@@ -1,6 +1,20 @@
 public class Gee17{
   public static void main(String[] args){
-    TwoDee twoDee = new TwoDee();
+
+    int[][] ray = new int[7][7];
+
+    int fredrick = 1;
+
+    for(row=0; row <ray.length; row++)
+    {
+      for(int col=0; col < ray[row].length; col++ )
+      {
+        ray[row][col] = fredrick;
+        fredrick++;
+      }
+    }
+
+    ArrayAverages twoDee = new ArrayAverages(ray);
 
     twoDee.rowAverages();
     twoDee.columnAverages();
